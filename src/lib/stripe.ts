7552cd10e580f932bb6d326409fd352dfcf9a1b6
Emmanuel Oduor <wyllademanu003@gmail.com>
2026-06-10 11:23:20 +0300

    lib: Stripe client configuration

---
+++ src/lib/stripe.ts
@@ -0,0 +1,9 @@
+import Stripe from "stripe";
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+export const PRO_PRICE_ID = process.env.STRIPE_PRO_PRICE_ID || "price_pro_monthly";
+
+export function getStripePublishableKey() {
+  return process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!;
+}
